ff7ca25d28705cc528b336fc6a4a2de4d2d9af36
car1sberg <deephell.best@gmail.com>
2018-05-26 01:18:32 +0300

    ADD: css to settings page

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,9 +1,27 @@
 
 import React from 'react';
+import '../css/Settings.css';
+import smile from '../icons/smile.svg';
+
+const smileIcon = <img src={smile} className="smile-icon" alt="smile" />
 
 const Settings = () => {
     return(
-        <div>Settings</div>
+        <div>
+            <aside>
+                <div className="anchors">
+                    {smileIcon}
+                    <span>People</span>
+                </div>
+                <div className="anchors">
+                    {smileIcon}
+                    <span>Appearence</span>
+                </div>
+                
+                
+            </aside>
+            <div></div>
+        </div>
     )
 }
 
--- src/components/SettingsHeader.js
+++ src/components/SettingsHeader.js
@@ -1,12 +1,20 @@
 
 import React, { Component } from 'react';
+import '../css/SettingsHeader.css';
 
-class Settings extends Component {
+class SettingsHeader extends Component {
     render() {
         return(
-            <h1>Settings Works!</h1>
+            <div className="settings-header">
+                <div className="settings-title">
+                    <h1>Settings</h1>
+                </div>
+                <div className="settings-input">
+                    <input type="text" placeholder="Search..." />
+                </div>
+            </div>
         )
     }
 }
 
-export default Settings;
+export default SettingsHeader;
