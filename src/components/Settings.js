dddfd4605244868d95839f8dd1a121c58887ab50
Car1sberg <deephell.best@gmail.com>
2018-05-26 13:56:12 +0300

    ADD: styles tu user block

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -35,11 +35,20 @@ const Settings = () => {
             </aside>
             <div className="settings-info">
                 <span className="user-title">People</span>
-                <div>
-                    <div></div>
-                    <div>
-                        <span>Manage other people</span>
+                <div className="default-settings-block">
+                    <div className="user-profile">
+                        <div className="user-avatar-block">
+                        <div className="avatar"></div>
+                        <div className="user-info">
+                            <span>Username</span>
+                            <span className="user-email">Email</span>
+                        </div>
+                        </div>
+                        <span className="edit-user">Edit</span>
                     </div>
+                </div>
+                <div className="manage-people">
+                    <span>Manage other people</span>
                 </div>
             </div>
         </div>
